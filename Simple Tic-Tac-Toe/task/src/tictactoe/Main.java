0924267d5587bb7e097a0016190b56ba031f6f2d
AlexseyProhin <alexsey.prohin@yandex.ru>
2022-05-10 20:56:32 +0300

    first commit

---
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -0,0 +1,18 @@
+package tictactoe;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String  displaysThemToTheUser = sc.nextLine();
+        System.out.println("---------");
+        System.out.println("| " + displaysThemToTheUser.charAt(0) + " " + displaysThemToTheUser.charAt(1) + " " + displaysThemToTheUser.charAt(2) + " |");
+        System.out.println("| " + displaysThemToTheUser.charAt(3) + " " + displaysThemToTheUser.charAt(4) + " " + displaysThemToTheUser.charAt(5) + " |");
+        System.out.println("| " + displaysThemToTheUser.charAt(6) + " " + displaysThemToTheUser.charAt(7) + " " + displaysThemToTheUser.charAt(8) + " |");
+        System.out.println("---------");
+        if ()
+        }
+    }
+}
